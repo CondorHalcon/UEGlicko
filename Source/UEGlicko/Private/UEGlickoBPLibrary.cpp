d7f602906cbd0f639843879640de8849bdc1a74e
CondorHalcon <judahhohawi@outlook.com>
2025-01-07 11:55:26 -0800

    Created Plugin

---
+++ Source/UEGlicko/Private/UEGlickoBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UEGlickoBPLibrary.h"
+#include "UEGlicko.h"
+
+UUEGlickoBPLibrary::UUEGlickoBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UUEGlickoBPLibrary::UEGlickoSampleFunction(float Param)
+{
+	return -1;
+}
+
