cbe7a1cca7ecd620202cd19a667e0cb20bd60c70
CondorHalcon <judahhohawi@outlook.com>
2025-01-07 17:56:54 -0800

    Wrote Base Update Function

--- Source/UEGlicko/Private/Rating.cpp
+++ Source/UEGlicko/Private/Rating.cpp
@@ -2,6 +2,7 @@
 
 #include "Rating.h"
 #include "Match.h"
+#include "Math/UnrealMathUtility.h"
 
 URating::URating()
 {
@@ -26,6 +27,32 @@ URating* URating::MakeRatingSimple()
 
 void URating::UpdateMatches(TArray<UMatch*> Matches)
 {
+	TArray<double> gTable = TArray<double>();
+	TArray<double> eTable = TArray<double>();
+	double invV = 0.0;
+
+	for (int i = 0; i < Matches.Num(); i++) {
+		URating* opponent = Matches[i]->getOpponent();
+		double g = opponent->G();
+		double e = opponent->E(g, this);
+
+		gTable[i] = g;
+		eTable[i] = e;
+		invV += g * g * e * (1.0 - e);
+	}
+
+	double v = 1.0 / invV;
+
+	double dInner = 0.0;
+	for (int j = 0; j < Matches.Num(); j++) {
+		dInner += gTable[j] * (Matches[j]->getScore() - eTable[j]);
+	}
+
+	double d = v * dInner;
+
+	ratingPending = FMath::Exp(Convergence(d, v, deviation, volatility) / 2.0);
+	deviationPending = 1.0 / FMath::Sqrt((1.0 / (deviation * deviation + ratingPending * ratingPending)) + invV);
+	volatilityPending = rating + deviationPending * deviationPending * dInner;
 }
 
 void URating::UpdateMatch(UMatch* Match)
@@ -44,42 +71,67 @@ void URating::Apply()
 {
 }
 
-float URating::getRating1()
+double URating::getRating1()
 {
-	return 0.0f;
+	return (this->rating * URating::Scale) + URating::DefaultRating;
 }
 
-float URating::getDeviation1()
+double URating::getDeviation1()
 {
-	return 0.0f;
+	return this->deviation * URating::Scale;
 }
 
-float URating::getVolatility1()
+double URating::getDelta1()
 {
-	return 0.0f;
+	return this->delta * URating::Scale;
 }
 
-float URating::getRating2()
+double URating::getRating2()
 {
-	return 0.0f;
+	return this->rating;
 }
 
-float URating::getDeviation2()
+double URating::getDeviation2()
 {
-	return 0.0f;
+	return this->deviation;
 }
 
-float URating::getVolatility2()
+double URating::getVolatility2()
 {
-	return 0.0f;
+	return this->volatility;
+}
+
+double URating::getDelta2()
+{
+	return this->delta;
 }
 
 FString URating::getGlicko1()
 {
-	return FString();
+	return FString::Printf(TEXT("[µ%02d:φ%02d]"), getRating1(), getDeviation1());
 }
 
 FString URating::getGlicko2()
 {
-	return FString();
+	return FString::Printf(TEXT("[µ%02d:φ%02d:σ%02d]"), getRating2(), getDeviation2(), getVolatility2());
+}
+
+double URating::G()
+{
+	return 0.0;
+}
+
+double URating::E(double g, URating* rating)
+{
+	return 0.0;
+}
+
+double URating::F(double x, double dS, double pS, double v, double a, double tS)
+{
+	return 0.0;
+}
+
+double URating::Convergence(double d, double v, double p, double s)
+{
+	return 0.0;
 }
--- Source/UEGlicko/Public/Rating.h
+++ Source/UEGlicko/Public/Rating.h
@@ -17,6 +17,13 @@ class UEGLICKO_API URating : public UObject
 public:
 	URating();
 
+	const double DefaultRating = 1500.0;
+	const double DefaultDeviation = 350.0;
+	const double DefaultVolatility = 0.06;
+	const double Scale = 173.7178;
+	const double SystemConst = 0.5;
+	const double Convergence = 0.000001;
+
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "Make Rating", Keywords = "Glicko Rating Make Rating Detailed"), Category = "UEGlicko|Rating")
 	static URating *MakeRating(double r = 1500, double d = 350, double v = 0.06);
 
@@ -39,22 +46,25 @@ public:
 	void Apply();
 
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Rating 1", Keywords = "Glicko Rating Get Rating 1"), Category = "UEGlicko|Rating")
-	float getRating1();
+	double getRating1();
 
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Deviation 1", Keywords = "Glicko Rating Get Deviation 1"), Category = "UEGlicko|Rating")
-	float getDeviation1();
+	double getDeviation1();
 
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Volatility 1", Keywords = "Glicko Rating Get Volatility 1"), Category = "UEGlicko|Rating")
-	float getVolatility1();
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Delta 1", Keywords = "Glicko Rating Get Delta 1"), Category = "UEGlicko|Rating")
+	double getDelta1();
 
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Rating 2", Keywords = "Glicko Rating Get Rating 2"), Category = "UEGlicko|Rating")
-	float getRating2();
+	double getRating2();
 
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Deviation 2", Keywords = "Glicko Rating Get Deviation 2"), Category = "UEGlicko|Rating")
-	float getDeviation2();
+	double getDeviation2();
 
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Volatility 2", Keywords = "Glicko Rating Get Volatility 2"), Category = "UEGlicko|Rating")
-	float getVolatility2();
+	double getVolatility2();
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Delta 2", Keywords = "Glicko Rating Get Delta 2"), Category = "UEGlicko|Rating")
+	double getDelta2();
 
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Glicko 1", Keywords = "Glicko Rating Get Glicko 1"), Category = "UEGlicko|Rating")
 	FString getGlicko1();
@@ -73,7 +83,7 @@ private:
 	double volatility;
 
 	UPROPERTY(VisibleAnywhere)
-	float delta;
+	double delta;
 
 	UPROPERTY(VisibleAnywhere)
 	TArray<UMatch *> matchesPending;
@@ -81,4 +91,9 @@ private:
 	double ratingPending;
 	double deviationPending;
 	double volatilityPending;
+
+	double G();
+	double E(double g, URating* rating);
+	static double F(double x, double dS, double pS, double v, double a, double tS);
+	static double Convergence(double d, double v, double p, double s);
 };
